0f5ef82ebee11b402d615b04e2f445bacd6482ab
Matt Noye (MWN Digital) <matt@mwn-digital.uk>
2024-07-28 19:08:05 +0100

    Additional to currency prev commit

---
+++ app/Models/Currency.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Currency extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'code',
+        'name',
+        'symbol',
+    ];
+
+    protected function user(){
+        return $this->belongsTo(User::class);
+    }
+}
