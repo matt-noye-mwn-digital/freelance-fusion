24296e82cf2dc5a670b27aecfdc9d84418350f2d
Matt Noye (MWN Digital) <matt@mwn-digital.uk>
2024-09-26 08:02:22 +0100

    Setup multi-level dropdowns in the sidebar and also the JS file

--- resources/views/admin/layouts/navs/sidebarNav.blade.php
+++ resources/views/admin/layouts/navs/sidebarNav.blade.php
@@ -3,20 +3,59 @@
         <a href="{{ route('admin.dashboard') }}" class="{{ Route::is('admin.dashboard') ? 'active' : '' }}">Dashboard</a>
     </li>
     <li class="dropdown">
+        <a class="dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">Billing</a>
+        <ul class="dropdown-menu">
+            <li><a href="">Transactions</a></li>
+            <li class="dropdown dropdown-submenu">
+                <a class="dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">Invoices</a>
+                <ul class="dropdown-menu submenu-dropdown-menu">
+                    <li><a href="">All Invoices</a></li>
+                    <li><a href="">Create Invoice</a></li>
+                    <hr>
+                    <li><a href="">Paid</a></li>
+                    <li><a href="">Unpaid</a></li>
+                    <li><a href="">Draft</a></li>
+                    <li><a href="">Cancelled</a></li>
+                    <li><a href="">Refunded</a></li>
+                    <li><a href="">Cancellations</a></li>
+                    <li><a href="">Payment Pending</a></li>
+                </ul>
+            </li>
+            <li class="dropdown dropdown-submenu">
+                <a class="dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">Billable Items</a>
+                <ul class="dropdown-menu"></ul>
+            </li>
+            <li><a href="">Quotes</a></li>
+        </ul>
+    </li>
+    <li class="dropdown">
         <a class="dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">Clients</a>
         <ul class="dropdown-menu">
-            <li><a href="{{--{{ route('admin.clients.index') }}--}}">All Clients</a></li>
-            <li><a href="{{--{{ route('admin.clients.create') }}--}}">Create Client</a></li>
+            <li><a href="{{ route('admin.clients.index') }}">All Clients</a></li>
+            <li><a href="{{ route('admin.clients.create') }}">Create Client</a></li>
+        </ul>
+    </li>
+    <li class="dropdown">
+        <a class="dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">Leads</a>
+        <ul class="dropdown-menu">
+            <li>
+                <a href="{{ route('admin.leads.index') }}">All Leads</a>
+            </li>
+            <li>
+                <a href="{{ route('admin.leads.create') }}">
+                    Create Lead
+                </a>
+            </li>
         </ul>
     </li>
     <li class="dropdown">
         <a class="dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">Staff</a>
         <ul class="dropdown-menu">
             <li>
-                <a href="{{--{{ route('admin.staff.index') }}--}}">All Staff</a>
+                <a href="{{ route('admin.staff.index') }}">All Staff</a>
             </li>
             <li>
-                <a href="{{--{{ route('admin.staff.create') }}--}}">Add Staff Member</a>
+                <a href="{{ route('admin.staff.create') }}">Add Staff Member</a>
             </li>
         </ul>
     </li>
