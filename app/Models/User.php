3be983f3fddfb7bf569770169c52a30cd1d2a051
Matt Noye (MWN Digital) <matt@mwn-digital.uk>
2024-09-30 13:13:23 +0100

    Updated the users model and migration to include full_name Updated the register controller to set full name from first and last name

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
     protected $fillable = [
         'first_name',
         'last_name',
+        'full_name',
         'email',
         'password',
         'avatar',
